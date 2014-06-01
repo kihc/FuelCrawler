9618843cbd541f8b4229be0c267ea02bcc48832c
Miha Sedej (prasuljo) <miha.sedej@gmail.com>
2014-06-01 14:50:08 +0200

    Nekaj sprememb

--- main.py
+++ main.py
@@ -94,9 +94,10 @@ class IndexHandler(webapp2.RedirectHandler):
 
         krneki = krneki.decode('UTF-8')
         krneki_horiz = krneki_horiz.decode('UTF-8')
-        podatki2 = "['Derivat', 'Ostalo', 'Mar\xc5\xbea distributerjev', '\xc4\x8clanarina ZRSBR', 'Tro\xc5\xa1arina', 'DDV (" + str(
-            (const_ddv - 1) * 100) + "%)'],"
-        podatki2 = podatki2.decode('UTF-8')
+
+        ###
+        # pripravimo podatke za barchart graf
+        ###
 
         json_barchart = {
             'cols':
@@ -111,112 +112,125 @@ class IndexHandler(webapp2.RedirectHandler):
             'rows': []
         }
 
-        #json_barchart['rows'].append({'c': [{'v': JSONdatum, 'f': 'null'}, {'v': str(i.temperatura), 'f': 'wtf'}, {'v': str(i.vlaga), 'f': 'wtf2'}]})
-
-
-
         for a, b, c, d, e in zip(db_zadnja_sprememba.drobnoProdajnaCena, db_zadnja_sprememba.trosarina, imena_derivatov, const_marze,
                                  const_zrsbr):
             ddv = a - a / const_ddv
             #               [derivat,   ostalo,             marža,          članarinaZRSBR, trošarina,      ddv          ]
-            podatki2 += str(
-                [c, round(a - b - d - e - ddv, 3), round(d, 3), round(e, 3), round(b, 3), round(ddv, 3)]) + ','
+
             json_barchart['rows'].append({'c': [{'v': c, 'f': c}, {'v': round(a - b - d - e - ddv, 3), 'f': str(round(a - b - d - e - ddv, 3))}, {'v': round(d, 3), 'f': str(round(d, 3))},{'v': round(e, 3), 'f': str(round(e, 3))},{'v': round(b, 3), 'f': str(round(b, 3))},{'v': round(ddv, 3), 'f': str(round(ddv, 3))}]})
 
-
-        podatki2 = podatki2[:-1]
-
-        podatki2 = '[' + podatki2 + ']'
-
-        #self.response.out.write(podatki2)
-
-        podatki = podatki2
-
-        ##temperatura
-        json_temperatura = {
+        ###
+        # pripravimo podatke za linijski (casovni) graf
+        ###
+        json_casovno = {
             'cols':
             [
                 {'id': '', 'label': 't', 'type': 'date'},
-                {'id': '', 'label': 'T', 'type': 'number'},
-                {'id': '', 'label': 'H', 'type': 'number'}
+                {'id': '', 'label': 'Cena', 'type': 'number'},
+                {'id': '', 'label': 'Trosarina', 'type': 'number'}
             ],
             'rows': []
         }
 
-        temperatura = "['time', 'Temperatura', 'Vlaga'],\n"
         for i in db_cene:
-            #temperatura = temperatura + "['" + str(i.date) + "', " + str(i.drobnoProdajnaCena[0]) + ", " + str(i.trosarina[0]) + "],\n"
-
             datum = datetime.strptime(str(i.date), '%Y-%m-%d %H:%M:%S.%f')
 
             JSONdatum = "Date(" + str(datum.year) + ", " + str(datum.month-1) + ", " + str(datum.day) + ", "
             JSONdatum += str(datum.hour) + ", " + str(datum.minute) + ", " + str(datum.second) + ")"
+            json_casovno['rows'].append({'c': [{'v': JSONdatum, 'f': 'null'}, {'v': str(i.drobnoProdajnaCena[0]), 'f': 'wtf'}, {'v': str(i.trosarina[0]), 'f': 'wtf2'}]})
 
+        ###
+        # pripravimo podatke za napoved
+        ###
 
-            json_temperatura['rows'].append({'c': [{'v': JSONdatum, 'f': 'null'}, {'v': str(i.drobnoProdajnaCena[0]), 'f': 'wtf'}, {'v': str(i.trosarina[0]), 'f': 'wtf2'}]})
-
-
+        json_napoved = {
+            'cols':
+            [
+                {'id': '', 'label': 'Datum', 'type': 'date'},
+                {'id': '', 'label': 'Vrednost', 'type': 'number'},
+                {'id': '', 'label': '% razlika', 'type': 'number'}
+            ],
+            'rows': []
+        }
 
         vrednost = []
+        cena_zdaj = []
+        cena_prej = []
 
         if len(db_tecaji_stari) > 0:
             for dan in db_tecaji_stari:
-                vrednost.append(dan.tecaj_usd*dan.tecaj_rbob_gasoline)
+                vrednost.append(dan.tecaj_usd * dan.tecaj_rbob_gasoline)
 
-            cena_prej = sum(vrednost)/float(len(vrednost))
-            test_payload = "Cena v prejsnjem obdobju: " + str(cena_prej)
+            cena_prej = sum(vrednost) / float(len(vrednost))
 
         if len(db_tecaji_aktualni) > 0:
             for dan in db_tecaji_aktualni:
                 vrednost.append(dan.tecaj_usd*dan.tecaj_rbob_gasoline)
 
-            test_payload += "\nCena v tekocem obdobju: " + str(sum(vrednost)/float(len(vrednost)))
+            cena_zdaj = sum(vrednost)/float(len(vrednost))
 
         db_tecaji = ndbTecaj.query(ndbTecaj.date > db_timemark.date - timedelta(days=30)).fetch()
 
-        test_payload += "\n<br />\n"
+        indeks_zadnja_spr = 0
+        indeks_predzadnja_spr = 0
+
         for p in db_tecaji:
 
             datum = p.date.strftime('%Y-%m-%d %H:%M')
-            vr = float(p.tecaj_rbob_gasoline)*float(p.tecaj_usd)
-            rel = 100.0*float((vr - cena_prej)/cena_prej)
+            vr = float(p.tecaj_rbob_gasoline) * float(p.tecaj_usd)
+            f_vr = str.strip("{:10.2f}".format(vr))
 
-            if p.date >= db_timemark.date:
-                test_payload += "<b>"
-            test_payload += "[ " + datum + " | " + "{:10.2f}".format(vr) + " | " + "{:10.2f}".format(rel) + "% ]"
+            rel = 100.0 * float((vr - cena_prej)/cena_prej)
+            f_rel = str.strip("{:10.2f}".format(rel))
 
-            if p.date >= db_timemark.date:
-                test_payload += "</b>"
+            if p.date < db_timemark.date - timedelta(days=1):
+                indeks_zadnja_spr += 1
 
-            test_payload += "<br />\n"
+            if p.date < db_timemark.date - timedelta(days=15):
+                indeks_predzadnja_spr += 1
 
+            json_to_append = {
+                'c':
+                    [
+                        {'v': datum, 'f': datum},
+                        {'v': f_vr, 'f': f_vr},
+                        {'v': f_rel, 'f': f_rel + "%"}
+                    ]
+            }
+
+            json_napoved['rows'].append(json_to_append)
+
+        #glede na stran na kateri smo, dolocimo kaj bomo poslali v template
+        if subpage == "/":
+            data_json = json_barchart
+        elif subpage == "/casovno":
+            data_json = json_casovno
+        elif subpage == "/podrobno":
+            data_json = json_torta
+        elif subpage == "/napoved":
+            data_json = json_napoved
+        else:
+            data_json = []
 
         template_values = {
-            'zadnjaSprememba': db_zadnja_sprememba,
-            'predZadnjaSprememba': db_predzadnja_sprememba,
-            'podatki': podatki,
-            'json_barchart': json_barchart,
+            'data_json': json.dumps(data_json, indent=2),
             'krneki': krneki,
-            'json_torta': json.dumps(json_torta),
             'subpage': subpage,
             'krneki_horiz': krneki_horiz,
-            'temperatura': temperatura,
-            'JSONtemperatura': json.dumps(json_temperatura),
             'datum': db_zadnja_sprememba.date,
-            'payload': test_payload
+            'napoved_cene': [cena_prej, cena_zdaj, indeks_zadnja_spr, indeks_predzadnja_spr],
         }
 
         template = jinja_environment.get_template('templates/neki.htm')
         self.response.out.write(template.render(template_values))
 
 
-class Krneki(webapp2.RequestHandler):
+class SiteVerification(webapp2.RequestHandler):
     def get(self):
         self.response.out.write("google-site-verification: googled67ece29abf3e179.html")
 
 
 class TecajiHandler(webapp2.RequestHandler):
-    @staticmethod
     def get(self):
         try:
             page_1 = urllib2.urlopen(url_tecaj_usd).read()
@@ -384,5 +398,5 @@ app = webapp2.WSGIApplication([('/osvezi', OsveziHandler),
                                ('/naroci', IndexHandler),
                                ('/napoved', IndexHandler),
                                ('/timemarker', TimeMarker),
-                               ('/googled67ece29abf3e179.html', Krneki)],
+                               ('/googled67ece29abf3e179.html', SiteVerification)],
                               debug=True)
